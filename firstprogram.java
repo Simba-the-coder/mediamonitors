5db7cee8bf212a07b61b35a6a4f8d308195e5cc8
Ryan <mzengeni66@gmail.com>
2021-02-22 12:28:54 +0200

    init

---
+++ firstprogram.java
@@ -0,0 +1,16 @@
+
+
+
+/**
+ * firstprogram
+ */
+public class firstprogram {
+
+    public static void main(String[] args) {
+
+        int arr [] = new int[]{1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println("The length of array is " + arr.length);
+
+    }
+}
